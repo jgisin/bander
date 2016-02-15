4b4e8a6ecc1432d6a31209f8768c3078ad33cb5f
Kit Langton <kitlangton@gmail.com>
2016-02-15 18:34:44 -0500

    fix host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host:'herokuapp.com' }
+  config.action_mailer.default_url_options = { host:'https://nameless-river-79882.herokuapp.com' }
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
